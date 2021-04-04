9c4cd7cace61defe12950084d2be42df64ebab20
aintyourcat <sqrt49is7@protonmail.com>
2021-04-04 21:56:16 +0800

    Add event listener for buttons and some refactor old code

--- script.js
+++ script.js
@@ -4,74 +4,115 @@ function computerPlay() {
     return pickedItem;
 }
 
-function formatPlayerSelection(playerSelection) {
-    playerSelection = playerSelection.charAt(0).toUpperCase() +
-	playerSelection.slice(1).toLowerCase();
-
-    return playerSelection;
-}
-
 function playRound(playerSelection, computerSelection) {
-    playerSelection = formatPlayerSelection(playerSelection);
-    let won;
+    let playerWin = null;
 
     switch (playerSelection) {
 	case 'Rock':
 	    switch (computerSelection) {
 		case 'Paper':
-		    won = false;
+		    playerWin = false;
 		    break
 		case 'Scissors':
-		    won = true;
+		    playerWin = true;
 		    break;
 	    }
 	    break;
 	case 'Paper':
 	    switch (computerSelection) {
 		case 'Rock':
-		    won = true;
+		    playerWin = true;
 		    break
 		case 'Scissors':
-		    won = false;
+		    playerWin = false;
 		    break;
 	    }
 	    break;
 	case 'Scissors':
 	    switch (computerSelection) {
 		case 'Rock':
-		    won = false;
+		    playerWin = false;
 		    break
 		case 'Paper':
-		    won = true;
+		    playerWin = true;
 		    break;
 	    }
 	    break;
-	default:
-	    return 'Input invalid!';
     }
+    
+    return playerWin;
+}
 
-    if (won === undefined) {
-	return 'Draw!';
-    } else if(won === true) {
-	return `You win!, ${playerSelection} beats ${computerSelection}`;
-    } else {
-	return `You lose!, ${computerSelection} beats ${playerSelection}`;
-    }
+function updatePlayerScore(playerScore) {
+    document.querySelector('#player-score').innerText = playerScore;
+}
+
+function updateComputerScore(computerScore) {
+    document.querySelector('#computer-score').innerText = computerScore;
+}
+
+function updateGameResult(gameResult) {
+    document.querySelector('#game-result').innerText = gameResult;
+}
+
+function disableItemButtons() {
+    const items = document.querySelectorAll('.item');
+    
+    items.forEach(item => {
+	if (!item.hasAttribute('disabled')) {
+	    item.setAttribute('disabled', '');
+	}
+    });
+}
+
+function enableItemButtons() {
+    const items = document.querySelectorAll('.item');
+
+    items.forEach(item => {
+	if (item.hasAttribute('disabled')) {
+	    item.removeAttribute('disabled');
+	}
+    });
 }
 
 function game() {
-    let playerSelection;
-    let gameCount = 1;
+    const items = document.querySelectorAll('.item');
 
-    while (gameCount <= 5) {
-	playerSelection = prompt('Your item?', '');
+    let playerScore = 0;
+    let computerScore = 0;
+    
+    items.forEach(item => {
+        item.addEventListener('click', event => {
+	    const playerSelection = item.dataset.name;
+	    const computerSelection = computerPlay();
+	    const playerWin = playRound(playerSelection, computerSelection);
 
-	if (!(playerSelection === null || playerSelection === '')) {
-	    console.log(playRound(playerSelection, computerPlay()));
-	}
+	    if (playerWin === true) {
+		updatePlayerScore(playerScore += 1);
+		updateGameResult(`Yippie! ${playerSelection} beat${(playerSelection === 'Scissors') ? '' : 's'} ${computerSelection}`);
+	    } else if(playerWin === false) {
+		updateComputerScore(computerScore += 1); 
+		updateGameResult(`Oh no! ${computerSelection} beat${(computerSelection === 'Scissors') ? '' : 's'} ${playerSelection}`);
+	    } else {
+		updateGameResult(`Draw! ${playerSelection} meet${(playerSelection === 'Scissors') ? '' : 's'} ${computerSelection}`);
+	    }
 
-	gameCount += 1;
-    }
+	    if (playerScore === 5 || computerScore === 5) {
+		gameResult = (playerScore === 5) ? 'You win!' : 'You lose!';
+		updateGameResult(gameResult);
+		disableItemButtons();
+	    }
+        });
+    });
+
+    const restartButton = document.querySelector('#restart');
+
+    restartButton.addEventListener('click', event => {
+	updatePlayerScore(playerScore = 0);
+	updateComputerScore(computerScore = 0);
+	updateGameResult('Choose your item');
+	enableItemButtons();
+    });
 }
 
 game();
